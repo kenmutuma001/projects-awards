8e28298aee954feae2c52a5862d5d17bdf9e158f
kennedymutuma <santa@northpole.com>
2019-05-26 19:49:51 +0300

    define search profile

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ken/Desktop/moringa-school-independent-projects/ken/Awards/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
