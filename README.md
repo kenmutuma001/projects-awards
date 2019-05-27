ef719f9ef5134119575c5ff4f6fdb13e77bd65f8
kennedymutuma <santa@northpole.com>
2019-05-27 17:47:18 +0300

    create the readme

---
+++ README.md
@@ -0,0 +1,53 @@
+## AWARDS
+
+## AUTHOR
+
+GITONGA KENNEDY MUTUMA
+
+## PROJECT DESCRIPTION
+
+This website allows user to view different projects,search users and rate other projects. 
+
+## TECHNOLOGIES USED
+
+ * ATOM editor
+ * GIT
+ * Django
+ * PYTHON3.6
+ * BOOTSTRAP
+ * CSS
+
+## SET_UP INSTRUCTIONS
+
+* Clone this repository
+* Create a virtual environment
+* Install django dependancies
+* Set the settings to development mode.
+*Run the python3.6 manage.py runserver
+
+## BDD
+
+ |       Behaviour                     | input                                  |   output              |
+ |------------|-----------|----------------|                    
+ | User searches	| user inputs the usename 	|returns user if available     | 
+| user enters site |clicks image	| displays a modal with details and vote option|
+
+## LINK
+https://ken-awards.herokuapp.com/
+## BUGS
+
+There are no known bugs if any is found please make contact
+
+## CONTACT
+
+kennedy.qen830@gmail.com
+
+## LINCENSE INFORMATION
+
+Copyright (c) [2019] [GITONGA KENNEDY MUTUMA]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- app/settings.py
+++ app/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jirm^o6+3mes#-umwmbmez!&swc0*eqqg^h(=h+grlmp)o&y-z'
+# SECRET_KEY = 'jirm^o6+3mes#-umwmbmez!&swc0*eqqg^h(=h+grlmp)o&y-z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ken-awards.herokuapp.com']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,20 +83,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tuzo',
-        'USER': 'ken',
-        'PASSWORD': 'kennedy',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tuzo',
+#         'USER': 'ken',
+#         'PASSWORD': 'kennedy',
+#     }
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -134,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
